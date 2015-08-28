1f66c06d843a35c6ac7ab127c9247c928256ea0b
Francis Wang <go717franciswang@hotmail.com>
2015-08-28 08:37:36 -0700

    Remove antialiasing

--- javascripts/minesweeper/load.js
+++ javascripts/minesweeper/load.js
@@ -1,6 +1,5 @@
 var loadState = {
     preload: function() {
-        //game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
         game.scale.pageAlignHorizontally = true;
 
         game.load.spritesheet('tile', '/resources/minesweeper/tiles.png', 10, 10, 15);
@@ -9,12 +8,14 @@ var loadState = {
         game.load.image('trophy', '/resources/minesweeper/trophy.png', 20, 20);
         game.load.image('back-arrow', '/resources/minesweeper/back-arrow.png', 20, 20);
 
-        //  Load the Google WebFont Loader script
+        // Load the Google WebFont Loader script
         game.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js');
+
+        // remove antialiasing
+        game.stage.smoothed = false;
     },
 
     create: function() {
-        game.stage.smoothed = false;
         game.state.start('play');
     }
 };
